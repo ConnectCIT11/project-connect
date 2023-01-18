40a17c7d00a502c8a06867056007dcb7b94598cd
Deyveson <deyvesonn@outlook.com>
2023-01-18 15:44:56 -0400

    service and controller

--- ws-cit/controllers/productController.js
+++ ws-cit/controllers/productController.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const finfById = require("../services/productService");
+
 
 router.get("/products", (req,res) => {
     res.send('Get product')
 });
 
-router.post("/product", (res,req) => {
-    res.send('Post product')
+router.get('/product/:id', (req, res) => {
+    res.send(finfById(req.params.id));
 });
-
 
 module.exports = router;
---
+++ ws-cit/services/productService.js
@@ -0,0 +1,6 @@
+const finfById = (id) => {
+    const products = ['1 dia', '2 dias', '3 dias']
+    return products[id];
+}
+
+module.exports = finfById;
