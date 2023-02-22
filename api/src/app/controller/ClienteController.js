bc185a5bc66bcd5c2ef5cc8fe46d0a3dc834a1f0
Deyveson <deyvesonn@outlook.com>
2023-02-22 14:21:37 -0400

    retirando espaços

--- api/src/app/controller/ClienteController.js
+++ api/src/app/controller/ClienteController.js
@@ -4,80 +4,73 @@ const Cliente = require("../model/Cliente");
 require('dotenv').config();
 
 class ClienteController {
-    async login(req, res){
 
-        const { email, senha } = req.body;
+  async login(req, res){
+    const { email, senha } = req.body;
+    const data = await Cliente.find({email, senha});
 
-        const data = await Cliente.find({email, senha});
+    if (data.length != 0) {
+      const jwt = require("jsonwebtoken");
+      const dadosUsuario = {
+        nome: data[0].nome,
+        email: data[0].email,
+        // id: data[0]._id
+      };
 
-        if (data.length != 0) {
-
-            const jwt = require("jsonwebtoken");
-
-            const dadosUsuario = {
-                nome: data[0].nome,
-                email: data[0].email,
-                // id: data[0]._id
-            };
-            
-            jwt.sign(dadosUsuario, process.env.CHAVE_PRIVADA, (err, token) => {
-                if (err) {
-                    res
-                        .status(500)
-                        .json({ mensagem: "Erro ao gerar o JWT" });
-
-                    return;
-                }
-
-                logger.info('Login sucesso')
-                res.set("x-access-token", token);
-                res.end();
-            });
-        } else {
-            
-            logger.warn('Login error')
-            res.status(401);
-            res.send({
-                erro: 401,
-                descricao: 'Nome ou senha invalidos'
-            })
-            res.end();
+      jwt.sign(dadosUsuario, process.env.CHAVE_PRIVADA, (err, token) => {
+        if (err) {
+          res
+          .status(500)
+          .json({ mensagem: "Erro ao gerar o JWT" });
+          return;
         }
 
+        logger.info('Login sucesso')
+        res.set("x-access-token", token);
+        res.end();
+      });
+    } else {
+      logger.warn('Login error')
+      res.status(401);
+      res.send({
+        erro: 401,
+        descricao: 'Nome ou senha invalidos'
+      })
+      res.end();
     }
+  }
 
-    getInfo(req, res){
+  getInfo(req, res){
+    const token = req.headers["authorization"]
+    const validacao = middlewareValidarJWT(token);
 
-        const token = req.headers["authorization"]
-        const validacao = middlewareValidarJWT(token);
-
-        if (!validacao.erro){
-            res.status(200);
-            return res.json({
-                code: 200,
-                descricao: 'Token validado com sucesso'
-            });
-          }else{
-            res.status(validacao.erro);
-            return res.json(validacao.descricao);
-          }
+    if (!validacao.erro){
+      res.status(200);
+      return res.json({
+          code: 200,
+          descricao: 'Token validado com sucesso'
+      });
+    }else{
+      res.status(validacao.erro);
+      return res.json(validacao.descricao);
     }
+  }
 
-    async store(req, res){
-        try{
-            const data = await Cliente.create(req.body);
-            logger.info('Cliente cadastrado com sucesso');
-            return res.json({data});
-        }catch{ 
-            logger.warn('Login error')
-            res.status(404);
-            res.send({
-                erro: 404,
-                descricao: 'Erro ao persistir cliente'
-            })
-            res.end();
-        }
+  async store(req, res){
+    try{
+      const data = await Cliente.create(req.body);
+      logger.info('Cliente cadastrado com sucesso');
+      return res.json({data});
+    }catch{ 
+      logger.warn('Login error')
+      res.status(404);
+      res.send({
+          erro: 404,
+          descricao: 'Erro ao persistir cliente'
+      })
+      res.end();
     }
+  }
 }
 
 
--- api/src/modules/Auth/AuthMiddleware.js
+++ api/src/modules/Auth/AuthMiddleware.js
@@ -2,29 +2,27 @@ const logger = require("../Log/Logger");
 
 const middlewareValidarJWT = (authorization) => {
 
-    if(authorization){
-        const chavePrivada = "conect.com.br";
-
-        const jwtService = require("jsonwebtoken");
-        return jwtService.verify(authorization, chavePrivada, (err, userInfo) => {
-            if (err) {
-                logger.warn('Token invalido');
-                return {
-                    erro: 403,
-                    descricao: 'Token invalido'
-                }
-            }
-            logger.info('Token validado com sucesso');
-            return userInfo
-        });
-    }else{
-        logger.warn('Sem token para validacao');
-        return {
-            erro: 401,
-            descricao: 'Sem token'
-        }
-    }
-    
+	if(authorization){
+		const chavePrivada = "conect.com.br";
+		const jwtService = require("jsonwebtoken");
+		return jwtService.verify(authorization, chavePrivada, (err, userInfo) => {
+			if (err) {
+				logger.warn('Token invalido');
+				return {
+					erro: 403,
+					descricao: 'Token invalido'
+				}
+			}
+			logger.info('Token validado com sucesso');
+			return userInfo
+		});
+	}else{
+		logger.warn('Sem token para validacao');
+		return {
+			erro: 401,
+			descricao: 'Sem token'
+		}
+	}
 };
 
 
